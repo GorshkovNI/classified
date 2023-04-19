089662792db4d41e18d6b4c56043f3701433c196
Gorshkov Nikita <nigorshkov99@gmail.com>
2023-04-20 00:04:19 +0500

    ProductInfo

--- src/Pages/ProductInfo/ProductInfo.jsx
+++ src/Pages/ProductInfo/ProductInfo.jsx
@@ -7,6 +7,7 @@ import qwe from "./qwe.jpg";
 import two from "./2.jpg";
 import three from "./3.jpg";
 import { Layout } from "../../component/Layout/Layout";
+import { Button } from "../../component/Button/Button";
 
 const images = [
   {
@@ -43,12 +44,11 @@ export const ProductInfo = ({ toggleModal, getId }) => {
               <div className={styles.titleInfoMain}>
                 <div>
                   <h1 className={styles.titleInfo}>
-                    Полиуретановая плёнка для фар
+                    Skoda Octavia A7, 2020
                   </h1>
                 </div>
                 <div className={styles.titleActions}>
-                  <button>123</button>
-                  <button>1234</button>
+                  <Button className={styles.actionButton} mode="outlined" type='text' icon='love' classNameIcon={styles.buttonIconLove} >Добавить в избранное</Button>
                 </div>
               </div>
               <div className={styles.viewMainContent}>
@@ -58,6 +58,80 @@ export const ProductInfo = ({ toggleModal, getId }) => {
                   showFullscreenButton={false}
                   additionalClass={styles.gallery}
                 />
+              </div>
+              <div className={styles.descriptionBlock}>
+                <div className={styles.parametrs}>
+                  <h2 className={styles.parametrsSpan}>
+                    Характеристики
+                  </h2>
+                  <ul className={styles.parametrsList}>
+                    <li className={styles.itemUl}>
+                      <span className={styles.item}>Год выпуска: </span>
+                      2020
+                    </li>
+                    <li className={styles.itemUl}>
+                      <span className={styles.item}>Поколение: </span>
+                      III
+                    </li>
+                    <li className={styles.itemUl}>
+                      <span className={styles.item}>Пробег: </span>
+                      159000км
+                    </li>
+                    <li className={styles.itemUl}>
+                      <span className={styles.item}>ПТС </span>
+                      Электронный
+                    </li>
+                    <li className={styles.itemUl}>
+                      <span className={styles.item}>Владельцев по ПТС:  </span>
+                      1
+                    </li>
+                    <li className={styles.itemUl}>
+                      <span className={styles.item}>Модификация:  </span>
+                      2.0 TDI 4WD DSG (184 л.с.)
+                    </li>
+                    <li className={styles.itemUl}>
+                      <span className={styles.item}>Объём двигателя:  </span>
+                      2.0 л
+                    </li>
+                    <li className={styles.itemUl}>
+                      <span className={styles.item}>Коробка передач:  </span>
+                      Робот
+                    </li>
+                    <li className={styles.itemUl}>
+                      <span className={styles.item}>Привод:  </span>
+                      Полный
+                    </li>
+                    <li className={styles.itemUl}>
+                      <span className={styles.item}>Комплектация:  </span>
+                      Базовая
+                    </li>
+                    <li className={styles.itemUl}>
+                      <span className={styles.item}>Тип кузова:  </span>
+                      Универсал
+                    </li>
+                    <li className={styles.itemUl}>
+                      <span className={styles.item}>Цвет:  </span>
+                      Серый
+                    </li>
+                    <li className={styles.itemUl}>
+                      <span className={styles.item}>Руль:  </span>
+                      Левый
+                    </li>
+                    <li className={styles.itemUl}>
+                      <span className={styles.item}>VIN или номер кузова:  </span>
+                      TMBL*************
+                    </li>
+                  </ul>
+                  
+                </div>
+                <div className={styles.parametrs}> 
+                    <h2 className={styles.parametrsSpan}>
+                      Описание
+                    </h2>
+                    <pre>
+                      Какое-то описание для машины
+                    </pre>
+                </div>
               </div>
             </div>
             <div className={styles.itemContent_right}></div>
