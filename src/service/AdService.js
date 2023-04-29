a82319377ab65b79a9707611556fdfce71a70587
Gorshkov Nikita <nigorshkov99@gmail.com>
2023-04-29 18:55:04 +0500

    1

--- src/service/AdService.js
+++ src/service/AdService.js
@@ -4,4 +4,10 @@ export default class AdService{
     static async getTypes(type){
         return $api.get('add-item/getTypes')
     }
+
+    static async getFieldsCategory(category){
+        return $api.post('add-item/getFieldsType', {category})
+    }
+
+
 }
--- src/store/ad/adSlice.js
+++ src/store/ad/adSlice.js
@@ -5,6 +5,7 @@ import AdService from "../../service/AdService";
 const initialState = {
     type: '',
     isAdLoading: false,
+    isError: false,
     categories: [],
     fields: []
 
@@ -32,14 +33,23 @@ const ad = createSlice({
         },
 
         setFields(state, action){
+            console.log(action)
+            state.fields = [...action.payload]
+        },
 
+        setError(state){
+            state.isError = true
+        },
+
+        removeError(state){
+            state.isError = false
         }
     }
 })
 
 export const getCategories = () => async (dispatch) => {
     try{
-        dispatch(fetchDataStart())
+        dispatch(fetchAdDataStart())
         const response = await AdService.getTypes()
         const category = response.data.map((item) => {
             return {
@@ -49,20 +59,28 @@ export const getCategories = () => async (dispatch) => {
         })
         console.log(response)
         dispatch(setCategories(category))
-        dispatch(fetchDataSuccess())
+        dispatch(fetchAdDataSuccess())
     } catch (e){
         console.log(e)
     }
 }
 
-export const getFields = () => async (dispatch) => {
+export const getCategoryFields = (category) => async (dispatch) => {
     try {
-        console.log(123)
+        dispatch(fetchAdDataStart())
+        const response = await AdService.getFieldsCategory(category)
+        console.log(response)
+        if(response.data.fields?.length === 0){
+            dispatch(setError())
+            dispatch(fetchDataSuccess())
+            return
+        }
+        dispatch(setFields(response.data.fields))
+        dispatch(fetchAdDataSuccess())
     } catch (e){
         console.log(e)
     }
 }
 
-
-export const { fetchAdDataStart, fetchAdDataSuccess, setType, setCategories } = ad.actions
+export const { fetchAdDataStart, fetchAdDataSuccess, setType, setCategories, setFields, setError, removeError } = ad.actions
 export default ad.reducer
